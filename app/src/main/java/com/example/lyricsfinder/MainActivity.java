4bbda090899812a7365bed0b6111c80671688b49
RomeniaSilva <sromeniasilva@gmail.com>
2022-12-04 16:54:22 +0530

    lyrics api added

--- app/src/main/java/com/example/lyricsfinder/MainActivity.java
+++ app/src/main/java/com/example/lyricsfinder/MainActivity.java
@@ -3,12 +3,67 @@ package com.example.lyricsfinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edt_artist, edt_song;
+    TextView tex_lyrics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edt_artist = findViewById(R.id.txtArtist);
+        edt_song = findViewById(R.id.txtSong);
+
+        tex_lyrics = findViewById(R.id.textView);
+
+        findViewById(R.id.btnFind).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://api.lyrics.ovh/v1/" + edt_artist.getText().toString() + "/" + edt_artist.getText().toString();
+                url.replace(" ","20%");
+
+                RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+                //requesting json data from url
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try {
+                            tex_lyrics.setText(response.getString("lyrics"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+                requestQueue.add(jsonObjectRequest);
+            }
+        });
     }
 }
